e789e25b693e3eee0cd7f98ef8267c1caaa7bedb
msbsoft2 <sadrabroo@gmail.com>
2023-11-29 19:00:30 +0330

    first commit

--- pages/api/category.js
+++ pages/api/category.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
     try {
-        const response = await prisma.Category.findMany();
+        const response = await prisma.category.findMany();
         res.status(200).json(response)
     } catch (error) {
         res.status(500).json({ msg: error.message })
--- pages/api/products.js
+++ pages/api/products.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
     try {
-        const response = await prisma.Product.findMany();
+        const response = await prisma.product.findMany();
         res.status(200).json(response)
     } catch (error) {
         res.status(500).json({msg: error.message})
